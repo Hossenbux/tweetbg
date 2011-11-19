56da5da72563647f846c7dd6f02640dbed52dd43
vovko <vladimir@drizhepolov.com>
2011-11-19 11:53:10 -0500

    debug index.php

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php 
-
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 /*
  * Look for a picture from 500px based on a keyword
+ * 
+ * 
+ */
+
+require_once("500pxAPI/index.php");
+
+echo pic500("pizza");
+
+/*
  * 
  * Select a random pic from the list with rating more than 1
  * 
