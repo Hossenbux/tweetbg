8497dea554a6544b69d990dce6b6ea3e60dd0fa2
vovko <vladimir@drizhepolov.com>
2011-11-19 14:47:47 -0500

    Build tiles, save into file, upload to twitter

--- 500pxAPI/index.php
+++ 500pxAPI/index.php
@@ -4,19 +4,17 @@
  * User has successfully authenticated with Twitter. Access tokens saved to session and DB.
  */
 
-function pic500($keyword, $consumer_key="xHkW9aeTnoYk4k1lUYicCjbKY9VXjYOWxE3OsBt8", $consumer_sectet="SoxoUAwEOuV2lSQKLWRcj5Tm2LM4X1l4hMlr2Skc" )
+function pic500($keyword, $consumer_key="xHkW9aeTnoYk4k1lUYicCjbKY9VXjYOWxE3OsBt8", $consumer_secret="SoxoUAwEOuV2lSQKLWRcj5Tm2LM4X1l4hMlr2Skc" )
 {
-	
-	session_start();
+
 	require_once('twitteroauth/twitteroauth.php');
 	require_once('config.php');
 
-	$access_token = $_SESSION['access_token'];
 
-	$connection = new TwitterOAuth($consumer_key, $consumer_secret, $access_token['oauth_token'], $access_token['oauth_token_secret']);
+	$connection = new TwitterOAuth($consumer_key, $consumer_secret, null, null);
 	
 	/* If method is set change API call made. Test is called by default. */
-	$content  = $connection->get('photos/search', array('term' => 'baseball', 'rpp' => '99'));
+	$content  = $connection->get('photos/search', array('term' => $keyword, 'rpp' => '99'));
 	
 	$rate = 0;
 	$rate_k = 0;
@@ -27,19 +25,45 @@ function pic500($keyword, $consumer_key="xHkW9aeTnoYk4k1lUYicCjbKY9VXjYOWxE3OsBt
 	foreach($content->photos as $photo)
 	{
 		
-		if ($photo->rating > 1) { 
+		if ($photo->rating > 5) { 
 			$rated_arr[] = $k; 
 		}
 		$k++;
 	}
 	
-	$img_url_ = $content->photos[array_rand($rated_arr)]->image_url;
-	$img_url = str_replace("2.jpg", "4.jpg", $img_url_);
+	$img_url = $content->photos[array_rand($rated_arr)]->image_url;
+	//$img_url = str_replace("2.jpg", "4.jpg", $img_url_);
 	
 	return $img_url;
 	
 }
 
+function pix500tile($keyword)
+{
+	
+	$j_l =3;
+	$i_l =3;
+	
+	$new_image = imagecreatetruecolor(140*$i_l, 140*$j_l);
+	for ($i = 0; $i < $i_l; $i++)
+	{
+		for ($j = 0; $j < $j_l; $j++)
+		{
+				$img = null;
+			$img = imagecreatefromjpeg(pic500($keyword));
+			
+			imagecopyresampled($new_image, $img, ($i)*140, ($j)*140, 0, 0, 140, 140, 140, 140);
+		}
+	}
+		
+	
 
+    
+    $imgid = uniqid();
+    $path = "imgs/$imgid.jpg";
+	imagepng($new_image, $path);
+	
+	return $path;
+}
 
 
--- index.php
+++ index.php
@@ -1,35 +1,97 @@
 <?php 
+
+/*
+ * 
+ * 
+ * 
+ * TWEETBG.0BYTES.INFO
+ * 
+ * 
+ * 
+Database name :	db391902173
+Host name :	db391902173.db.1and1.com
+Port :	3306
+User name :	dbo391902173
+Description :	tweetbg: tweetbg
+Version :	MySQL5.0
+Status :	 setup started
+ * 
+ * 
+ * 
+ */
+
+
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 /*
  * Look for a picture from 500px based on a keyword
- * 
- * 
  */
 
 require_once("500pxAPI/index.php");
 
-echo pic500("pizza");
+$keyword = $_REQUEST['key'];
+
 
 /*
- * 
- * Select a random pic from the list with rating more than 1
- * 
- * Download a pic
- * 
- * Upload the pic to User's twitter BG
- * 
+ * Get a tile of pics from 500px
+ */
+
+$pic_url  = pix500tile($keyword);
+echo "<img src='$pic_url' />";
+
+
+/*  
+ * Upload the pic to User's twitter BG 
+
+ */
+require_once 'TwitterAPI/tweetbg.php';
+
+tweetbg($pic_url);
+
+/* 
  * Relax
- * 
  */
 
 
 
+/*
+ * UTILS
+ */
+
+
+
+// Get image
+
+function curlImg($img, $fullpath)
+{
+	$img = imagecreatefromjpeg($img);
+	imagejpeg($img, $fullpath);
+	
+	$new_image = imagecreatetruecolor(1024, $height);
+    imagecopyresampled($new_image, $this->image, 0, 0, 0, 0, $width, $height, $this->getWidth(), $this->getHeight());
+    $this->image = $new_image;
+	
+}
 
 
 
 
 
+//Alternative Image Saving Using cURL
+function save_image($img,$fullpath){
+    $ch = curl_init ($img);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_BINARYTRANSFER,1);
+    $rawdata=curl_exec($ch);
+    curl_close ($ch);
+    if(file_exists($fullpath)){
+        unlink($fullpath);
+    }
+    $fp = fopen($fullpath,'x');
+    fwrite($fp, $rawdata);
+    fclose($fp);
+}
 
 
 
