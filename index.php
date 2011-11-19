967544f8542db127b9e0f296317841bc1959f8e0
vovko <vladimir@drizhepolov.com>
2011-11-19 11:39:03 -0500

    comment

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php 
 
 /*
+ * Look for a picture from 500px based on a keyword
  * 
+ * Select a random pic from the list with rating more than 1
  * 
+ * Download a pic
  * 
+ * Upload the pic to User's twitter BG
  * 
- * 
+ * Relax
  * 
  */
 
