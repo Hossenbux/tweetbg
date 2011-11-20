01127da06d0e06d8c17a048f634e62e1ab794e2e
Adrian Maurer <me@adrianissocial.com>
2011-11-19 23:29:14 -0500

    update last tweet record, delete temp collage

--- cron.php
+++ cron.php
@@ -28,7 +28,7 @@ while($row = mysql_fetch_array($sources))
 		try {
 			$oauth->fetch("https://api.twitter.com/1/statuses/user_timeline.json?screen_name=$name&since_id=$last_id&trim_user=true"); 
 			$json = json_decode($oauth->getLastResponse());	
-			getTweets($name, $last_id, $json, $row);
+			getTweets($name, $last_id, $json, $row, $tweet['last_keyword']);
 		
 		} catch (Exception $e){
 			//remove user record
@@ -41,48 +41,53 @@ while($row = mysql_fetch_array($sources))
 
 echo 'done';
 
-function getTweets($name, $last_id, $json, $row){
+function getTweets($name, $last_id, $json, $row, $last_keyword){
 				
 	foreach($json as $single){
 		preg_match('/([a-zA-Z0-9_-]+)\*/', $single->text, $matches);
+		
 		if($matches){
 			$keyword = str_replace('*', '', $matches[0]);
-			echo $keyword;
-			require_once('500pxAPI/index.php');
-			require_once('tmhOAuth.php');
-			require_once('tmhUtilities.php');
-			$fullPath = pix500tile($keyword);
 			
-			var_dump($row);
-			var_dump($fullPath);
-			var_dump($keyword);
-			
-			$tmhOAuth = new tmhOAuth(array(
-			    'consumer_key'    => 'WMlOe7DvrfnQo58lzOTDdQ',
-			    'consumer_secret' => 'YZ4Ie9JEZhwflbbbwTslwyL0G7ZK2A1S0S2cUMbo',
-			    'user_token'      => $row['access_token'],
-			    'user_secret'     => $row['token_secret'],
-		    ));
+			if($keyword != $last_keyword) {
+				
+				require_once('500pxAPI/index.php');
+				require_once('tmhOAuth.php');
+				require_once('tmhUtilities.php');
+		
+				$fullPath = pix500tile($keyword);
+				
+				$tmhOAuth = new tmhOAuth(array(
+				    'consumer_key'    => 'WMlOe7DvrfnQo58lzOTDdQ',
+				    'consumer_secret' => 'YZ4Ie9JEZhwflbbbwTslwyL0G7ZK2A1S0S2cUMbo',
+				    'user_token'      => $row['access_token'],
+				    'user_secret'     => $row['token_secret'],
+			    ));
+							
+				$params = array(
+				    //'image' => "@{$_FILES['image']['tmp_name']};type={$_FILES['image']['type']};filename={$_FILES['image']['name']}",
+					'image' => "@$fullPath;type=JPEG",
+					'tile' => true,
+					'use'=> true
+			  	);
+	
+				$code = $tmhOAuth->request('POST', $tmhOAuth->url("1/account/update_profile_background_image"),
+				    $params,
+				    true, // use auth
+				    true  // multipart
+			  	);
+				
+				unlink($fullPath);
+				
+				mysql_query("UPDATE user_tweets SET last_keyword='$keyword', last_id=$single->id WHERE screen_name='$name'");
+				
+			   	// if ($code == 200) {
+			    	 // tmhUtilities::pr(json_decode($tmhOAuth->response['response']));
+			   	// }
+			   	// tmhUtilities::pr(htmlentities($tmhOAuth->response['response']));
 						
-			$params = array(
-			    //'image' => "@{$_FILES['image']['tmp_name']};type={$_FILES['image']['type']};filename={$_FILES['image']['name']}",
-				'image' => "@$fullPath;type=JPEG",
-				'tile' => true,
-				'use'=> true
-		  	);
-
-			$code = $tmhOAuth->request('POST', $tmhOAuth->url("1/account/update_profile_background_image"),
-			    $params,
-			    true, // use auth
-			    true  // multipart
-		  	);
-
-		   	// if ($code == 200) {
-		    	 // tmhUtilities::pr(json_decode($tmhOAuth->response['response']));
-		   	// }
-		   	// tmhUtilities::pr(htmlentities($tmhOAuth->response['response']));
-					
-			return;
+				return;
+			}
 		}
 	}
 }
