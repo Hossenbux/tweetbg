fe463ba8b9ab8f09f476366bdbf9aa15541ec57c
Adrian Maurer <me@adrianissocial.com>
2011-11-26 17:36:54 -0500

    updated .gitignore

--- application/controllers/builder.php
+++ application/controllers/builder.php
@@ -2,25 +2,20 @@
 
 class Auth extends TweetBG_Controller {
     
-    protected $req_url = 'https://api.twitter.com/oauth/request_token';
-    protected $authurl = 'https://api.twitter.com/oauth/authorize';
-    protected $acc_url = 'https://api.twitter.com/oauth/access_token';
-    protected $api_url = 'https://api.twitter.com';
-    protected $conskey = '';
-    protected $conssec = '';
-    
     function __construct() {
         parent::__construct();
         
         $this->load->library('session');
         $this->load->database();
+        $this->load->model('Authenticate');
         
         $keys = $this->db->query("SELECT * FROM consumer")->result();
 
-        $this->conskey = $keys[0]->consumer_key;
-        $this->conssec = $keys[0]->consumer_secret;
+        $this->Authenticate->conskey = $keys[0]->consumer_key;
+        $this->Authenticate->conssec = $keys[0]->consumer_secret;
     }
     
     function index() {
-        
+        die('nothing yet');
     }
+}
