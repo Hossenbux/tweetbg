a5b4970373093b3ae080af355326d496fd1320f1
Vladimir Drizhepolov <vladimir@drizhepolov.com>
2011-11-28 15:49:12 -0500

    User js

--- js/user.js
+++ js/user.js
@@ -1,11 +1,27 @@
  window.addEvent('domready', function(){
 
     document.getElement('.getSample').addEvent('click', function(event){
+    	
+    	var source =  document.getElements("[name=source]:checked").get("value");
+    	var sampleimg = document.getElement(".sample-img");
+    	var preload = new Element('img', {id: "loading","src":"/images/preload.gif"})
+    	document.getElement('.getSample').inject( preload , "after");
+           
+    	
         new Request({
-            'url': 'builder/sample/Flickr/pool',
+            'url': 'builder/sample/'+source+'/pool',
             'method': 'GET',
             'onSuccess': function(img) {
-                document.getElement('.sample').adopt(new Element('img', {'src': '/'+img}))
+            	
+            	if( sampleimg )
+            	{
+            		document.getElement(".sample-img").set("src", img)
+            	} 
+            		else 
+            	{
+            		document.getElement('.sample').adopt(new Element('img', { src: '/'+img, 'class': 'sample-img'}))
+            	}
+                document.getElement('#loading').destroy();
             }
         }).send();
     });
