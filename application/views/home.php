3632f25e875eaa8d31d293034b37e05c838ebc08
Adrian Maurer <me@adrianissocial.com>
2011-11-30 00:19:26 -0500

    instructions

--- application/views/home.php
+++ application/views/home.php
@@ -1,6 +1,6 @@
 <div class="container">
-    <a class="auth"><img src="/images/twitter.png"</a>
+    <div><a class="auth"><img src="/images/twitter.png"</a></div>
     
-    
+<iframe width="560" height="315" src="http://www.youtube.com/embed/aGB54wNBU0E" frameborder="0" allowfullscreen></iframe>    
     
 </div>
