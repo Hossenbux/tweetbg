d886834cfa5dab7a050835f877752b99ecfa51f1
Adrian Maurer <me@adrianissocial.com>
2011-12-05 18:22:46 -0500

    google analystics

--- application/views/home.php
+++ application/views/home.php
@@ -4,3 +4,17 @@
 <iframe width="560" height="315" src="http://www.youtube.com/embed/aGB54wNBU0E" frameborder="0" allowfullscreen></iframe>    
     
 </div>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-10664543-8']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
